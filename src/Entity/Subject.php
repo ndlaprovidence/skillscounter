47219054b25308dfb2b43febc6e0ff5aa4c24233
julien <julien.giroux50@gmail.com>
2021-03-25 11:40:21 +0100

    [CORE] Addition of the form as well as the storage of school subjects

---
+++ src/Entity/Subject.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="tbl_subject")
+ * @ORM\Entity(repositoryClass=SubjectRepository::class)
+ */
+class Subject
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+}
